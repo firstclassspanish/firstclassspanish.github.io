3e3aaf58f3c84eebf7df3f7ca04e9facd6f082ba
firstclassspanish <joshuamapp@gmail.com>
2026-07-16 13:53:52 -0700

    Add files via upload

--- js/animation-engine.js
+++ js/animation-engine.js
@@ -30,7 +30,7 @@ function initAnimation(durations, lessonUrl, onBeatShow) {
 
     if (current) {
       const animated = current.querySelectorAll(
-        '.messy-word, .box-cell, .vowel-item, .flash-words span, .sentence-line'
+        '.messy-word, .box-cell, .vowel-item, .flash-words span, .sentence-line, .word-block, .tag, .category-chip'
       );
       animated.forEach(el => {
         el.style.animation = 'none';
